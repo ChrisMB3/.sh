b23b1e412c7463b490f583f9d3d678ded5da0c43
Albin Lundin <75508593+albinlundin@users.noreply.github.com>
2020-12-05 12:55:58 +0100

    Delete lab4.sh

--- its20/lab4.sh
+++
@@ -1,9 +0,0 @@
-while [[ "$#" > 0 ]]; do case $1 in
- -tal1) tal1="$2"; shift;shift;;
- -tal2) tal2="$2";shift;shift;;
- *) echo "Unknown parameter passed: $1";shift;shift;;
-esac; done
-
-let sum=tal1+tal2
-
-echo Summan av talen är $sum
