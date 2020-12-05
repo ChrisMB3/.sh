257ebc96ff11bfd47c95d27e2720b169e0edf5ca
Albin Lundin <75508593+albinlundin@users.noreply.github.com>
2020-12-05 12:55:48 +0100

    Delete lab2.sh

--- its20/lab2.sh
+++
@@ -1,9 +0,0 @@
-while [[ "$#" > 0 ]]; do case $1 in
- -name) name="$2"; shift;shift;;
- -age) age="$2";shift;shift;;
- *) echo "Unknown parameter passed: $1";shift;shift;;
-esac; done
-
-let newAge=age+5
-
-echo Hej $name, om fem år är du $newAge år gammal.
